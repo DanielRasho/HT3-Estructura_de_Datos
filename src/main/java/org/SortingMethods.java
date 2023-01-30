a0960a14751c87a8db2e37618943a4ec3dbbcac6
Mónica Salvatierra <104535119+alee2602@users.noreply.github.com>
2023-01-30 15:07:01 -0600

    Crear clases
    SortingMethods para todos los tipos de Sorting y RandomNumberGenerator para generar los números al azar

---
+++ src/main/java/org/SortingMethods.java
@@ -0,0 +1,29 @@
+package org;
+
+import java.util.List;
+
+public class SortingMethods {
+
+    public <T> List<T> GnomeSort(List<Comparable<T>> listComparable) {
+        return null;
+    }
+
+    public <T> List<T> MergeSort(List<Comparable<T>> listMerge) {
+        return null;
+    }
+
+    public <T> List<T> QuickSort(List<Comparable<T>> listQuick) {
+        return null;
+
+    }
+
+    public <T> List<T> RadixSort(List<Comparable<T>> listRadix) {
+        return null;
+
+    }
+
+    public <T> List<T> ShellSort(List<Comparable<T>> listShell) {
+        return null;
+
+    }
+}
